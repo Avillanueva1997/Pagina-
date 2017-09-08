0437bcf0dfbad9a068703705b8b4250795711ebb
Avillanueva <a_ngel_456@hotmail.com>
2017-09-08 16:01:09 -0500

    last

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 <body>
 <div class="container-fluid">
 <div class="row">
-	<div class="col-md-12" style="padding-left: 0px; padding-right: 0px; width: 1334px;">
-<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="col-md-12" style="padding-left: 0px; padding-right: 0px;">
+<nav class="navbar navbar-inverse navbar-fixed-top" style="width: 1334px;">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
